a8bf2df9d563f378bdc1fe2e55766a9fb5c51a2f
nespolisimmaco <snespoli@yahoo.com>
2023-03-13 16:43:57 +0100

    Add: check if user's email is in the list of allowed emails

--- mail/js/script.js
+++ mail/js/script.js
@@ -6,7 +6,9 @@
 // Email dell'utente
 let userEmail;
 // Lista delle email di chi può accedere
-
+const allowedEmails = ["rossimario@gmail.com", "lucianospalletti@live.it", "simmaco@yahoo.com", "sn@outlook.com"];
+// Flag
+let isFound = false;
 // Quando clicco sul bottone "Invia", prendo l'email inserita nell'input e la salvo nella variabile userEmail
 const submitButton = document.getElementById("submit-btn");
 submitButton.addEventListener ("click", function () {
@@ -14,4 +16,27 @@ submitButton.addEventListener ("click", function () {
     console.log(userEmailInput);
     userEmail = userEmailInput.value;
     console.log(userEmail);
+
+    // Controllo che la mail inserita sia nella lista di chi può accedere
+    // SE l'input non è vuoto
+    //  confronto
+    // ALTRIMENTI
+    //   bisogna inserire una mail nell'input
+    if (userEmail !== "") {
+        for (let i = 0; i < allowedEmails.length; i++) {
+            console.log(allowedEmails[i]);
+            if (allowedEmails[i] === userEmail) {
+                isFound = true;
+            }
+            console.log(isFound);
+        }
+    } else {
+        alert("Inserire una mail!");
+    }
+
+    // Stampo un messaggio sull'esito del controllo
+    // SE isFound = true 
+    // l'email è autorizzata
+    // ALTRIMENTI
+    // l'email non è autorizzata
 })
